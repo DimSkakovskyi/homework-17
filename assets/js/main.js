a0b67f4f3c4e3f4b4a77023a77c8b54abffac248
DimSkakovskyi <dimonskak@ukr.net>
2025-07-07 12:20:18 +0300

    Finished second recording

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	$('.header__navbar-btn').click(function(){
 		$(this).toggleClass('active');
+		$('.header__navbar-list').stop(true, true).fadeToggle(500);
 	});
 });
